3bc423dee70147e330035bc24f61aa52478b433d
Julisa <julisa.lapa@utec.edu.pe>
2022-05-29 22:08:02 -0500

    delete all points

--- tk-try/tk.py
+++ tk-try/tk.py
@@ -17,10 +17,19 @@ def draw_line(event):
    # Draw an oval in the given co-ordinates
    canvas.create_oval(x1,y1,x2,y2,fill="black", width=20)
 
+def delete_line(event):
+   x1=event.x
+   y1=event.y
+   x2=event.x
+   y2=event.y   
+   # Delete all points
+   canvas.delete('all')
+
 # Create a canvas widget
 canvas=Canvas(win, width=700, height=350, background="white")
 canvas.grid(row=0, column=0)
 canvas.bind('<Button-1>', draw_line)
+canvas.bind('<Button-3>', delete_line)
 click_num=0
 
 win.mainloop()
